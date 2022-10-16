bf8c76aa2fed08f973d0a8bcc79559311c24cf22
CodingOneBitAtATime <96848990+WillisHH@users.noreply.github.com>
2022-10-16 23:29:49 +0100

    first completed homework

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -4,13 +4,13 @@
 TASK 1
 
 Write a class to represent a Cash Register.
-You class should keep the state of price total and purchased items
+Your class should keep the state of price total and purchased items
 
 Below is a starter code:
 ------------------------
 1. you can add new variables and function if you want to
 2. you will NEED to add accepted method parameters where required.
-For example, method add_item probably accepts some kind of an item?..
+For example, method add_item probably accepts some kind of item?..
 3. You will need to write some examples of how your code works.
 
 """
@@ -18,32 +18,56 @@ For example, method add_item probably accepts some kind of an item?..
 
 class CashRegister:
 
-    def __init__(self):
+    def __init__(self, total_items):
 
-        self.total_items = None # {'item': 'price'}
+        self.total_items = dict(total_items)  # {'item': 'price'}
         self.total_price = 0
-        self.discount = 0
+        self.discount = .5
 
-    def add_item(self):
-        pass
+    def add_item(self, item, price):
+        self.total_items.update({item: price})
+        print("Item added:", item)
 
-    def remove_item(self):
-        pass
+
+
+    def remove_item(self, item):
+        self.total_items.pop(item)
+        print("Item removed:", item)
+
+
 
     def apply_discount(self):
-        pass
+        discounted_amount = sum(self.total_items.values()) * self.discount
+        print(f"Your discount of {self.discount}% has been applied your total is: £{discounted_amount}")
+        return discounted_amount
+
 
     def get_total(self):
-        pass
+        self.total_price = 0
+        return print(f"Your total is: £{sum(self.total_items.values())}")
+
+
 
     def show_items(self):
-        pass
+        print(f"The items in your cart are:{self.total_items}")
 
     def reset_register(self):
-        pass
+        self.total_items.clear
+        self.discount = 0
+        self.total_price = 0
+        print(f"Register cleared:{self.total_items}")
+
+
 
 
 # EXAMPLE code run:
+basket_1 = CashRegister(total_items={'apple': 3.00, 'banana': 1.00, 'mango': 2.30, 'peach': 3.10, 'bread': 1.50})
 
-# ADD
+basket_1.add_item('orange', 2.00)
+basket_1.remove_item('apple')
+basket_1.get_total()
+basket_1.show_items()
+basket_1.apply_discount()
+
+
 
--- homework-1/student.py
+++ homework-1/student.py
@@ -1,11 +1,11 @@
-TASK 2
-
-Write a base class to represent a student. Below is a starter code.
-Feel free to add any more new features to your class.
-As a minimum a student has a name and age and a unique ID.
-
-Create a new subclass from student to represent a concrete student doing a specialization, for example:
-Software Student and Data Science student.
+# TASK 2
+#
+# Write a base class to represent a student. Below is a starter code.
+# Feel free to add any more new features to your class.
+# As a minimum a student has a name and age and a unique ID.
+#
+# Create a new subclass from student to represent a concrete student doing a specialization, for example:
+# Software Student and Data Science student.
 
 """
 
@@ -20,7 +20,97 @@ class Student:
 
 
 # class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subects (add/remove new subject and its graade to the dict)
+#     create new methods that manage student's subects (add/remove new subject and its grade to the dict)
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
+
+"""
+from statistics import mean
+
+class Student:
+
+    def __init__(self, name, age, id):
+        self.name = name
+        self.age = age
+        self.id = id
+        self.subjects = dict()
+
+    def student_name(self):
+        return print(f"The students full name is {self.name}")
+
+
+    def student_information(self):
+        return print(f"ID number {self.id}, Details: {self.name}, {self.age} years old")
+
+
+class CFGStudent(Student):
+
+    def __init__(self, name, age, id, specialization, subjects=None):
+        super().__init__(name, age, id)
+        self.specialization = specialization
+        self.subjects = subjects or dict()
+
+
+    def add_subject(self, subjects, grade):
+        self.subjects.update({subjects: grade})
+        return subjects
+
+
+    def remove_subject(self,subjects):
+        self.subjects.pop(subjects)
+        return subjects
+
+
+    def total_subjects(self):
+        return print(self.subjects)
+
+    def average_grade(self):
+        average_mark = (sum(self.subjects.values())/len(self.subjects))
+        return print(f"The average mark for {self.name} is {average_mark}")
+
+
+#Class of students
+student_1 = CFGStudent(name="Harry", age=10, id=1001, specialization="wizard")
+student_2 = CFGStudent(name="Ron", age=11, id=1002, specialization="wizard")
+student_3 = CFGStudent(name="Hermoine", age=9, id=1003, specialization="witch", subjects={"pottery": 10})
+student_4 = CFGStudent(name="Hagrid", age=22, id=1004, specialization="grounder")
+
+#student_1
+student_1.student_name()
+student_1.student_information()
+student_1.add_subject("potions", 5)
+student_1.add_subject("games", 10)
+student_1.add_subject("gardening", 4)
+student_1.remove_subject("gardening")
+student_1.total_subjects()
+student_1.average_grade()
+
+#student_2
+student_2.student_name()
+student_2.student_information()
+student_2.add_subject("potions", 9)
+student_2.add_subject("games", 2)
+student_2.add_subject("gardening", 10)
+student_2.remove_subject("potions")
+student_2.total_subjects()
+student_2.average_grade()
+
+#student_2
+student_3.student_name()
+student_3.student_information()
+student_3.add_subject("dance", 5)
+student_3.add_subject("potions", 10)
+student_3.remove_subject("potions")
+student_3.total_subjects()
+student_3.average_grade()
+
+#student_4
+student_4.student_name()
+student_4.student_information()
+student_4.add_subject("dance", 2)
+student_4.add_subject("forestry", 10)
+student_4.add_subject("gardening", 10)
+student_4.total_subjects()
+student_4.average_grade()
+
 
